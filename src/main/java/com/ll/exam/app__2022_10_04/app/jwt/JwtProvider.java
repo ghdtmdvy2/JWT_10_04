ccc78dc303b55b5b5b1d745ffe250ab5e5ffffab
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-05 13:26:50 +0900

    accessToken 인증 및 claims 얻기 추가 설명 2

--- src/main/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
+++ src/main/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
@@ -24,11 +24,15 @@ public class JwtProvider {
 
     public String generateAccessToken(Map<String, Object> claims, int seconds) {
         long now = new Date().getTime();
+        // 만료 날짜 정의.
         Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
 
         return Jwts.builder()
+                // 데이터를 json 형태로 만들기.
                 .claim("body", Util.json.toStr(claims))
+                // 만료 날짜 설정.
                 .setExpiration(accessTokenExpiresIn)
+                // SecretKey 로 JWT 비번 걸고, HS512 로 암호화
                 .signWith(getSecretKey(), SignatureAlgorithm.HS512)
                 .compact();
     }
@@ -36,6 +40,7 @@ public class JwtProvider {
     public boolean verify(String token) {
         try {
             Jwts.parserBuilder()
+                    // SecretKey 로 변조 되었는지 확인.
                     .setSigningKey(getSecretKey())
                     .build()
                     .parseClaimsJws(token);
@@ -48,10 +53,13 @@ public class JwtProvider {
 
     public Map<String, Object> getClaims(String token) {
         String body = Jwts.parserBuilder()
+                // SecretKey 로 변조 되었는지 확인.
                 .setSigningKey(getSecretKey())
                 .build()
+                // accessToken (JWT 토큰) 으로 claims(메인 데이터) 가져오기.
                 .parseClaimsJws(token)
                 .getBody()
+                // 어느 데이터 부분만 가져올지 결정. body는 메인 데이터를 뜻함.
                 .get("body", String.class);
 
         return Util.json.toMap(body);
